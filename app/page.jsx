86b19e2ddb23310521cb13e0d5b0fda953c164b4
Khan Ali <mohdalikhans97.com@gmail.com>
2024-08-18 04:23:33 +0400

    Infobox Hero and Footer Added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 import Link from "next/link";
 
 const HomePage = () => {
     return <div>
-        <h1 className="text-3xl">Welcome</h1>
-        <Link href='properties'>Go to properties</Link>
+        <Hero/>
+        <InfoBoxes/>
     </div>
 }
 
